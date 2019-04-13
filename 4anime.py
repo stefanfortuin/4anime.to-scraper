3945a84e5e287647f2533c2d4ceda909b64b6c9f
Stefan Fortuin <stefanfortuin02@gmail.com>
2019-04-13 22:09:40 +0200

    refactored some code and added a info function to gather info from a show

--- 4anime.py
+++ 4anime.py
@@ -11,9 +11,51 @@ def get_page_soup(url):
 	""" Returns a beautifulsoup object from the given url"""
 	page = requests.get(url)
 	soup = bs(page.content, "html.parser")
+
+	if soup.find("h1", {"class": "page-title"}) is not None:
+		print("Show not found")
+		exit()
+
 	return soup
 
-def get_all_episodes_from_show(show):
+def info_printer(info):
+	print(f"Title: \t\t {info['title'].replace('-', ' ').capitalize()}")
+	print(f"Episodes: \t {info['num_eps']}")
+	print(f"Genres: \t {info['genres']}")
+	print(f"Type: \t\t {info['details'][0]}")
+	print(f"Studio: \t {info['details'][1]}")
+	print(f"Release Date: \t {info['details'][2]}")
+	print(f"Status: \t {info['details'][3]}")
+	print(f"Language: \t {info['details'][4]}")
+
+
+def get_show_info(show):
+	""" Returns the info of a show """
+
+	# Build the show url
+	parsed_show = show.lower().replace(" ", "-")
+	show_url = main_url + show_suffix.format(parsed_show)
+
+	info_soup = get_page_soup(show_url)
+
+	genres_string = "" 
+	for genres in info_soup.find("div", {"class": "tags-mobile"}).findAll("a"):
+		for genre in genres.contents:
+			genres_string += f"{genre} "
+
+	details = [] 
+	for detail_div in info_soup.find("div", {"class": ["details", "flat-panel"]}).findAll("div", {"class", "detail"}):
+		d = ""
+		for detail in detail_div.findAll("a"):
+			d += f"{detail.contents[0]} "
+		details.append(d)
+	
+	num_of_episodes = len(info_soup.find("ul", {"class": ["episodes", "range", "active"]}).findAll("a"))
+
+	info_printer({"title":show, "num_eps":num_of_episodes, "genres":genres_string, "details":details})
+	exit()
+
+def get_episodes_from_show(show, episode=None):
 	""" Download all the episodes from a given show """
 
 	# Build the show url
@@ -27,35 +69,25 @@ def get_all_episodes_from_show(show):
 	links = show_soup.find("ul", {"class": ["episodes", "range", "active"]}).findAll("a")
 	episode_urls = [l.get('href') for l in links]
 
-	for i, episode in enumerate(episode_urls):
-		print("downloading episode: " + str(i))
-		download_episode(episode)
+	if episode is not None:
+		if len(episode_urls) < episode: 
+			print("Episode not found")
+			exit()
 
-def get_one_episode_from_show(show, episode):
-	""" Download a specific episode from a show."""
-	# Build the show url
-	show_url = main_url + show_suffix.format(show)
-
-	# Get the show page
-	show_soup = get_page_soup(show_url)
-
-	# Acquire the episode url from the show page
-	array_index = int(episode) - 1
-	links = show_soup.find("ul", {"class": ["episodes", "range", "active"]}).findAll("a")
-	episode_url = links[array_index].get('href')
-	
-	print("downloading episode: " + episode)
-	download_episode(episode_url)
+		ep_url = episode_urls[episode - 1]
+		download_episode(ep_url)
+	else:
+		for episode in episode_urls:
+			download_episode(episode)
 
 def download_episode(episode_url):
 	""" Downloads the episode from the url """
-
-	episode_page = requests.get(episode_url)
-	episode_soup = bs(episode_page.content, "html.parser")
+	episode_soup = get_page_soup(episode_url)
 
 	episode_url = episode_soup.find("source").get('src')
 	file_name = re.search(r"me(.+)", episode_url).group(1)
 
+	print("downloading episode: " + file_name)
 	downloader.get_file(episode_url, file_name)
 
 def get_arguments(args=None):
@@ -63,23 +95,22 @@ def get_arguments(args=None):
 
 	parser = argparse.ArgumentParser(description="4anime.to downloader")
 	parser.add_argument("--show", help="The show to download")
+	parser.add_argument("--info", help="The show to download")
 	parser.add_argument("--episode", help="A specific episode, default: all")
 	parser.add_argument("--threads", help="Number of maximum threads")
 
-	results = parser.parse_args(args)
-	return (results.show,
-			results.episode,
-			results.threads)
+	r = parser.parse_args(args)
+	return (r.show, r.info, r.episode, r.threads)
 
 if __name__ == "__main__":
-	show, episode, threads = get_arguments(sys.argv[1:])
+	show, info, episode, threads = get_arguments(sys.argv[1:])
+
+	if info is not None:
+		get_show_info(info)
 
 	if threads is None:
 		downloader = FileDownloader()
 	else:
 		downloader = FileDownloader(max_threads=int(threads))
 	
-	if episode is None:
-		get_all_episodes_from_show(show)
-	else:
-		get_one_episode_from_show(show, episode)
+	get_episodes_from_show(show, episode=int(episode))
